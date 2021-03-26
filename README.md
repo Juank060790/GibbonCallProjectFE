378605649ae96a7f74c0b4ff1afb843f27969998
Nick Doan <nickbar01234@gmail.com>
2021-03-26 16:48:06 +0700

    EDA

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
-* Last updated: 03/25/2021
-* Changes: README.md, test.ipynb. Begin extracting labelled data. 
-
+* Last updated: 03/26/2021
+* Changes: 
+     - extract.py: Added helper functions
+     - **TODO**: 
+          1) Analyze data. Most likely will have to fix the sliding 
+          window/hopping length according to our data (see extract.py).
+          2) Run data extraction and see how many gibbon segments we have. 
+* NOTE: 
+     - The **BIG** problem with our dataset is that we're assuming input 
+     segments only contain gibbon, bird + insects, individually. In reality, 
+     audio segments may overlapped. 
+     - Solutions:
+          1) Sound synthesis (could be risky).
+          2) Autoencoder (detect the nature of gibbon call).
+          3) Independent component analysis 
+          https://www.youtube.com/watch?v=T0HP9cxri0A
+          - The idea would be to run this algorithm (possibly model) before 
+          prediction and feed separated audio frequency into our prediction 
+          model. 
+---
 **Label Keys**:
 * **TODO**: Include a spectrogram sample for each type of call.
 * **NOTE**: *boom* and *coda* calls are not labelled. 
@@ -9,13 +26,16 @@
 * coda: Special type of *mm* call which the male only makes in response to the female great call
 * gc: great call. These are made by female gibbons. Immatures may also join in but will be quieter than the adult female(s). It consists of a series of very rapid calls that build in frequency and loudness. The male coda call comes in at the end.
 
-
 **Directory Path**:
 ```
 .
-|--- Raw Audio Files 
-     |--- Contains unextracted audio data
-|--- Selection Tables
-     |--- Contains labels 
-|---README.md
+|--- _helper 
+     |--- extract.py: Helper functions to extract raw audio files. 
+|--- _Raw Audio Files
+     |--- **\*.WAV, raw audio files.
+|--- _Selection Tables
+     |--- *.txt, labelled segments in an audio.
+|--- README.md
+|--- extraction.ipynb: Tests extracting audio files 
+|--- EDA.ipynb: Analysis of data. 
 ```
---
+++ helper/extract.py
@@ -0,0 +1,77 @@
+import pathlib 
+import os
+import regex as re
+import librosa
+import pandas as pd 
+import numpy as np
+
+def readLabels(path: str, sample_rate: int):
+    '''
+    * Purpose: Given a path to a text file containing the labels, read in the 
+    information and convert it to a dataframe. 
+    * Parameters: 
+        path: str. Path to file containing the labels. 
+        sample_rate: int. Audio sampling rate. 
+    * Returns:  
+        df: pd.DataFrame. Columns are start and end timestamps converted in 
+        sample rate, path to audio, and the label.
+    '''
+
+    def convertPath(path: str):
+        '''
+        * Purpose: Convert a path to absolute path.
+        '''
+        relative_path = re.findall("Raw Audio Files.+", path)[0]
+        return os.path.join(os.getcwd(), re.sub("\((.)*\)", \
+                        "(September to October 2020)", relative_path))
+
+    df = pd.read_csv(path, delimiter = "\t")
+    df.drop(columns = ["Selection", "View", "Channel", "Begin File"], 
+            inplace = True)
+    df.columns = ["Start", "End", "Low", "Height", "Path", "Label"]
+
+    df["Path"] = df["Path"].apply(convertPath)
+    df["Start"] = df["Start"] * sample_rate
+    df["End"] = df["End"] * sample_rate
+
+    return df 
+
+def extractAudio(df: pd.DataFrame, sample_rate: int, 
+                 alpha: int = 10, jump_seconds: int = 1):
+    '''
+    * Purpose: Extract calls using a sliding window approach.
+    * Parameters:
+        - df: pd.DataFrame. Contains timestamps and labels.
+        - sample_rate: int. Audio sampling rate. 
+        - alpha: int. How many seconds to slide over a given timestamp. 
+        - jump_seconds: int. Hops between sliding window. 
+    * Returns: 
+        - 
+    '''
+
+    audio, _ = librosa.load(df["Path"][0], sr = sample_rate)
+
+    keys = ["gc", "mm", "sc"]
+    alpha_converted = alpha * sample_rate 
+    gibbon = []
+
+    for _, row in df.iterrows():
+
+        if row["Label"] not in keys:
+            continue 
+
+        jump = 0
+        while True:
+            start_position = row["Start"] - sample_rate - \
+                             (jump * jump_seconds * sample_rate)
+            end_position = start_position + alpha_converted 
+            jump += 1
+
+            if end_position <= row["End"]:
+                break 
+            
+            gibbon.append(audio[int(start_position):int(end_position)])
+    
+    return np.asarray(gibbon, dtype = "object")
+
+
