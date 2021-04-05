6a80d75861043a819e9df4c1ff493b81382c27df
nickbar01234 <nickbar01234@gmail.com>
2021-04-05 10:02:22 +0700

    prediction

--- prediction/post_processing.py
+++ prediction/post_processing.py
@@ -1,6 +1,25 @@
-'''
-Steps:
-    1) Load test audio.
-    2) Get predictions:
-        - Predict in 
-'''
+import numpy as np 
+
+def timeIndex(alpha: int, file_durations: int):
+    '''
+    * Get the time indices (in seconds) of the total number of segments 
+    from a given audio file.
+    * Parameters:
+        - alpha: Number of seconds to extract.
+        - file_durations: Total duration of the file (in seconds).
+    '''
+
+    start = []
+    end = []
+
+    #If the file only last 10s, then we get at least 1 segment.
+    segments = int(file_durations - alpha + 1)
+
+    if (segments < 0):
+        raise ValueError(f"Negative segment values: {segments}")
+    
+    for i in range(segments):
+        start.append(i)
+        end.append(i + alpha)
+    
+    return np.asarray(start), np.asarray(end)
